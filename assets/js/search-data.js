47d52c9ee2f82ce3102521243fc91a9edee44157
Gabbzgreat <148372090+Gabbzgreat@users.noreply.github.com>
2025-12-07 03:17:27 +0000

    Deploying to gh-pages from @ Gabbzgreat/Gabbzgreat.github.io@cfa999009b4175bf14d7c36fc044f6c16b46a32a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "Design and simulation of a cascaded DC–DC + DC–AC converter with PI control, unipolar PWM, and 3 kW power delivery.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-plc-based-centrifuge-control-system",
+          title: 'PLC-Based Centrifuge Control System',
+          description: "A state-machine driven automation system for controlling a laboratory centrifuge with safety logic and HMI integration.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
