18f28345e1212bef2c575f95351b0bc11124666b
Gabbzgreat <148372090+Gabbzgreat@users.noreply.github.com>
2026-01-02 21:36:38 +0000

    Deploying to gh-pages from @ Gabbzgreat/Gabbzgreat.github.io@b990706fbe714c6c5d6dcd8c6ba82827f40f8602 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,16 +424,6 @@ ninja.data = [{
           description: "A state-machine driven automation system for controlling a laboratory centrifuge with safety logic and HMI integration.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
